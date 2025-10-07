f04df2de8564925581fd00ceabf814a967881e7e
Rupsha-github <rupshasaha3002@gmail.com>
2025-10-07 18:07:55 +0530

    Nodemailer changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,12 +10,15 @@ dotenv.config();
 // Helper to send OTP email
 async function sendOtpEmail(to, otp, context = 'Verify your email') {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // TLS
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000 // Optional: 10s timeout
+});
 
   await transporter.sendMail({
     from: '"Digilocker" <no-reply@digilocker.com>',
