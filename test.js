4e2b82ec5287abaeacc579c306c5a7349fe95276
Rupsha-github <rupshasaha3002@gmail.com>
2025-10-07 23:40:26 +0530

    Added gif

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ async function testResendEmail() {
   try {
     const response = await resend.emails.send({
       from: 'Digilocker <onboarding@resend.dev>',
-      to: process.env.TEST_EMAIL || 'rupshasaha3002@gmail.com',
+      to: process.env.TEST_EMAIL || 'dattatreya.dump@gmail.com',
       subject: 'Test Email from Resend',
       text: 'This is a test email sent using Resend API.'
     });
